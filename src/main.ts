4b2ebde42427536c612912bae3fb2667981110d0
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2026-04-15 11:32:59 +0200

    a

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ import {
   parseVoltageFromReading,
   rgb565ToHtml,
 } from "./config";
+import logoUrl from "./Logo SVG White.svg?url";
 import { WebSerialDevice, XModem } from "./serial";
 import "./style.css";
-import logoUrl from "./Logo SVG White.svg?url";
 
 // Constants
 const DISCONNECT_DELAY_MS = 1000;
@@ -42,7 +42,7 @@ function generateArcColorInputs(colors: string[]): string {
 function createUI(): void {
   document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <div class="container">
-      <h1><img src="${logoUrl}" alt="Lascar logo" class="header-logo"><span>Lascar Config Changer Revision 2</span></h1>
+      <h1><span>Lascar Config Changer Revision 2</span><img src="${logoUrl}" alt="Lascar logo" class="header-logo"></h1>
       
       <div class="section import-export-section">
         <h2>Import / Export Configuration</h2>
